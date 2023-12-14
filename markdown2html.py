81633b8152e50dd457dd53b699fddd62bc64b6cc
Kwabena Boakye <kwaboakye93@gmail.com>
2023-12-14 19:28:23 +0000

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,49 +1,39 @@
 #!/usr/bin/python3
 """
-This script converts markdown syntax html syntax.
+A script that codes markdown to HTML
 """
-import os
 import sys
-
-
-def parse_heading(string):
-    heading = {
-        "# ": "h1",
-        "## ": "h2",
-        "### ": "h3",
-        "#### ": "h4",
-        "##### ": "h5",
-        "###### ": "h6",
-    }
-    for i in heading.keys():
-        if line.startswith(i):
-            open_tag = f"<{heading[i]}>"
-            closing_tag = f"</{heading[i]}>"
-            actual_sentence = string.replace(i, "")
-
-            return f"{open_tag}{actual_sentence}{closing_tag}\n"
-
+import os
+import re
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
+    # Test that the number of arguments passed is 2
+    if len(sys.argv[1:]) != 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
-    md_file, html_file = sys.argv[1:]
+    # Store the arguments into variables
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-    if not os.path.exists(md_file) and not os.path.isfile(md_file):
-        print("Missing <filename>", file=sys.stderr)
+    # Checks that the markdown file exists and is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
 
-    with open(md_file, "r") as md_str:
-        md_str_list = md_str.readlines()
-        html_result_list = []
+    with open(input_file, encoding="utf-8") as file_1:
+        html_content = []
+        md_content = [line[:-1] for line in file_1.readlines()]
+        for line in md_content:
+            heading = re.split(r"#{1,6} ", line)
+            if len(heading) > 1:
+                # Compute the number of the # present to
+                # determine heading level
+                h_level = len(line[: line.find(heading[1]) - 1])
+                # Append the html equivalent of the heading
+                html_content.append(f"<h{h_level}>{heading[1]}</h{h_level}>\n")
+            else:
+                html_content.append(line)
 
-        md_str_list = [line.replace("\n", "") for line in md_str_list]
-        for line in md_str_list:
-            line = parse_heading(line)
-            html_result_list.append(line)
-
-        with open(html_file, "w") as html:
-            html.writelines(html_result_list)
+    with open(output_file, "w", encoding="utf-8") as file_2:
+        file_2.writelines(html_content)
